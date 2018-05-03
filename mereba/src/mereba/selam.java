db20f6e7afafda77cc5c5f8b1c82e0ea05781c3c
Kenan <kenancum@gmail.com>
2018-05-03 11:22:57 +0300

    aleyküm selam

--- mereba/src/mereba/selam.java
+++ mereba/src/mereba/selam.java
@@ -1,9 +1,10 @@
+package mereba;
 
-public class bilgeclass {
+public class selam {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Benim adim bilge");
+		System.out.println("hello");
 	}
 
 }
